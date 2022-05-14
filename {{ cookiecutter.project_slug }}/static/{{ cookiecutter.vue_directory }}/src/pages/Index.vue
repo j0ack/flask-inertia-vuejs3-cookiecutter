e74378a8817ad0a55b4a09001f45a60300e1ae64
TROUVERIE Joachim <jtrouverie@joakode.fr>
2022-05-14 10:33:03 +0200

    fix: python requirements

--- {{ cookiecutter.project_slug }}/static/{{ cookiecutter.vue_directory }}/src/pages/Index.vue
+++ {{ cookiecutter.project_slug }}/static/{{ cookiecutter.vue_directory }}/src/pages/Index.vue
@@ -3,7 +3,9 @@
     <img class="logo" src="https://flask.palletsprojects.com/en/2.0.x/_static/flask-icon.png" alt="Flask logo">
     <img class="logo" src="https://avatars.githubusercontent.com/u/47703742?s=200&v=4" alt="Inertia logo">
     <img class="logo" src="https://vuejs.org/images/logo.png" alt="Vue logo">
-    <h1 class="title">Welcome to your Flask-Inertia Vue app.</h1>
+    {% raw %}
+    <h1 class="title">Welcome to your Flask-Inertia {{ frameworkName }} app.</h1>
+    {% endraw %}
     <p class="info">
       For more information please visit the
       <a href="https://flask-inertia.readthedocs.io/">documentation</a>
@@ -16,6 +18,11 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'Index',
+  data() {
+    return {
+      frameworkName: 'Vue'
+    }
+  }
 });
 </script>
 
